7f59047aa000d12ba38dafe59d329f912c7ec311
sasestevens <sase@stevens.edu>
2019-07-01 11:26:17 -0400

    hopefully a fix

--- js/template.js
+++ js/template.js
@@ -11,32 +11,7 @@
 	$(document).ready(function(){
 	
 		$(".banner-image").backstretch('images/banner.jpg');
-	
 		
-		var slideIndex = 1;
-		showSlides(slideIndex);
-
-// Next/previous controls
-		function plusSlides(n) {
-			showSlides(slideIndex += n);
-		}
-
-// Thumbnail image controls
-		function currentSlide(n) {
-			showSlides(slideIndex = n);
-		}
-
-		function showSlides(n) {
-		  var i;
-		  var slides = document.getElementsByClassName("banner-image");
-		  var dots = document.getElementsByClassName("dot");
-		  if (n > slides.length) {slideIndex = 1} 
-		  if (n < 1) {slideIndex = slides.length}
-		  for (i = 0; i < slides.length; i++) {
-			  slides[i].style.display = "none"; 
-		  }
-		  slides[slideIndex-1].style.display = "block"; 
-		}
 		
 		// Fixed header
 		//-----------------------------------------------
