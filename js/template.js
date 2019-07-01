203d69d898fad818fa2961e0290b1481bd15b2b7
sasestevens <sase@stevens.edu>
2019-07-01 10:56:56 -0400

    umm

--- js/template.js
+++ js/template.js
@@ -11,6 +11,32 @@
 	$(document).ready(function(){
 	
 		$(".banner-image").backstretch('images/banner.jpg');
+		$(".banner-image1").backstretch('images/banner-1.jpg');
+		
+		var slideIndex = 1;
+		showSlides(slideIndex);
+
+// Next/previous controls
+		function plusSlides(n) {
+			showSlides(slideIndex += n);
+		}
+
+// Thumbnail image controls
+		function currentSlide(n) {
+			showSlides(slideIndex = n);
+		}
+
+		function showSlides(n) {
+		  var i;
+		  var slides = document.getElementsByClassName("banner-image");
+		  var dots = document.getElementsByClassName("dot");
+		  if (n > slides.length) {slideIndex = 1} 
+		  if (n < 1) {slideIndex = slides.length}
+		  for (i = 0; i < slides.length; i++) {
+			  slides[i].style.display = "none"; 
+		  }
+		  slides[slideIndex-1].style.display = "block"; 
+		}
 		
 		// Fixed header
 		//-----------------------------------------------
