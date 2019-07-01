86ff3aca608fcb2c443f089fac0e47a9d2f7f935
sasestevens <sase@stevens.edu>
2019-07-01 11:05:52 -0400

    i hope this works?

--- js/template.js
+++ js/template.js
@@ -10,33 +10,34 @@
 (function($){
 	$(document).ready(function(){
 	
-		$(".banner-image").backstretch('images/banner.jpg');
-		$(".banner-image1").backstretch('images/banner-1.jpg');
-		
-		var slideIndex = 1;
-		showSlides(slideIndex);
+var slideIndex = 1;
+showSlides(slideIndex);
 
 // Next/previous controls
-		function plusSlides(n) {
-			showSlides(slideIndex += n);
-		}
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
 // Thumbnail image controls
-		function currentSlide(n) {
-			showSlides(slideIndex = n);
-		}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-		function showSlides(n) {
-		  var i;
-		  var slides = document.getElementsByClassName("banner-image");
-		  var dots = document.getElementsByClassName("dot");
-		  if (n > slides.length) {slideIndex = 1} 
-		  if (n < 1) {slideIndex = slides.length}
-		  for (i = 0; i < slides.length; i++) {
-			  slides[i].style.display = "none"; 
-		  }
-		  slides[slideIndex-1].style.display = "block"; 
-		}
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
 		
 		// Fixed header
 		//-----------------------------------------------
