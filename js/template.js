6d395028ae4e8662ebb3100389b4c94c00822400
sasestevens <sase@stevens.edu>
2019-07-01 11:17:07 -0400

    idk

--- js/template.js
+++ js/template.js
@@ -10,34 +10,33 @@
 (function($){
 	$(document).ready(function(){
 	
-var slideIndex = 1;
-showSlides(slideIndex);
+		$(".banner-image").backstretch('images/banner.jpg');
+	
+		
+		var slideIndex = 1;
+		showSlides(slideIndex);
 
 // Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+		function plusSlides(n) {
+			showSlides(slideIndex += n);
+		}
 
 // Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+		function currentSlide(n) {
+			showSlides(slideIndex = n);
+		}
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
-}
+		function showSlides(n) {
+		  var i;
+		  var slides = document.getElementsByClassName("banner-image");
+		  var dots = document.getElementsByClassName("dot");
+		  if (n > slides.length) {slideIndex = 1} 
+		  if (n < 1) {slideIndex = slides.length}
+		  for (i = 0; i < slides.length; i++) {
+			  slides[i].style.display = "none"; 
+		  }
+		  slides[slideIndex-1].style.display = "block"; 
+		}
 		
 		// Fixed header
 		//-----------------------------------------------
